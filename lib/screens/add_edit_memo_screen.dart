c07fe0fcd5cd77bfcd7cfc892963dd78d05a7cc4
satoshi <uchiyama.s@timeshappy.com>
2022-10-08 10:51:42 +0900

    MAC版で作り直し。レイアウト調整

--- lib/screens/add_edit_memo_screen.dart
+++ lib/screens/add_edit_memo_screen.dart
@@ -4,12 +4,27 @@ import 'package:flutter/material.dart';
 import 'package:memo/models/memo.dart';
 
 class AddEditMemoScreen extends StatelessWidget {
-  final Memos? currentMemo;
   AddEditMemoScreen({Key? key, this.currentMemo}) : super(key: key);
+
+  final Memos? currentMemo;
 
   TextEditingController titleController = TextEditingController();
   TextEditingController heightController = TextEditingController();
   TextEditingController weightController = TextEditingController();
+  TextEditingController stateOfNutritionController = TextEditingController();
+  TextEditingController spinalColumnNoteController = TextEditingController();
+  TextEditingController rightEyeController = TextEditingController();
+  TextEditingController leftEyeController = TextEditingController();
+  TextEditingController rightCorrectedEyeController = TextEditingController();
+  TextEditingController leftCorrectedEyeController = TextEditingController();
+  TextEditingController eyeDiseaseController = TextEditingController();
+  TextEditingController earDiseaseController = TextEditingController();
+  TextEditingController tuberculosisController = TextEditingController();
+  TextEditingController tuberculosisDayController = TextEditingController();
+  TextEditingController ecgController = TextEditingController();
+  TextEditingController urineNoteController = TextEditingController();
+  TextEditingController othersController = TextEditingController();
+  TextEditingController other2Controller = TextEditingController();
 
   Future<void> save() async {
     final memoCollection = FirebaseFirestore.instance.collection('memoTest');
@@ -17,13 +32,25 @@ class AddEditMemoScreen extends StatelessWidget {
       'title': titleController.text,
       'height': heightController.text,
       'weight': weightController.text,
+      'stateOfNutrition': stateOfNutritionController.text,
+      'spinalColumnNote': spinalColumnNoteController.text,
+      'rightEye': rightEyeController.text,
+      'leftEye': leftEyeController.text,
+      'rightCorrectedEye': rightCorrectedEyeController.text,
+      'leftCorrectedEye': leftCorrectedEyeController.text,
+      'eyeDisease': eyeDiseaseController.text,
+      'earDisease': earDiseaseController.text,
+      'ecg': ecgController.text,
+      'urineNote': urineNoteController.text,
+      'other': othersController.text,
+      'other2': other2Controller.text,
       'createdTime': Timestamp.now(),
     });
   }
 
   Future<void> upData() async {
     final doc =
-        FirebaseFirestore.instance.collection('memoTest').doc(currentMemo!.id);
+    FirebaseFirestore.instance.collection('memoTest').doc(currentMemo!.id);
     await doc.update({
       'title': titleController.text,
       'height': heightController.text,
@@ -32,82 +59,289 @@ class AddEditMemoScreen extends StatelessWidget {
     });
   }
 
-  void initState() {
+/* void initState() {
     if (currentMemo != null) {
       titleController.text = currentMemo!.title;
-      titleController.text = currentMemo!.height;
-      titleController.text = currentMemo!.weight;
+      heightController.text = currentMemo!.height;
+      weightController.text = currentMemo!.weight;
     }
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
+    //initState();
     return Scaffold(
       appBar: AppBar(
         title: Text(currentMemo == null ? '新規作成' : '編集'),
       ),
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            const Text('title'),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                ),
-                child: TextField(
-                  controller: titleController,
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    contentPadding: EdgeInsets.only(left: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 5),
+              const Text('タイトル'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: titleController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Text('身長入力'),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                ),
-                child: TextField(
-                  controller: heightController,
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    contentPadding: EdgeInsets.only(left: 10),
+              const SizedBox(height: 5),
+              const Text('身長入力'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: heightController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Text('体重'),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                ),
-                child: TextField(
-                  controller: weightController,
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
-                    contentPadding: EdgeInsets.only(left: 10),
+              const SizedBox(height: 5),
+              const Text('体重'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: weightController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
                   ),
                 ),
               ),
-            ),
-            ElevatedButton(
-                onPressed: () async {
-                  Navigator.pop(context);
-                  await save();
-                },
-                child: const Text('保存')),
-          ],
+              const SizedBox(height: 5),
+              const Text('栄養状態'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: stateOfNutritionController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('脊柱及び胸郭の疾病及び異常の有無　並び四肢の状態'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: spinalColumnNoteController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('裸眼視力　右'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: rightEyeController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('裸眼視力　左'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: leftEyeController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('矯正視力　右'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: rightCorrectedEyeController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('矯正視力　左'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: leftCorrectedEyeController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('目の疾病'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: eyeDiseaseController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('耳の疾病'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: earDiseaseController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('結核の日付'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: other2Controller,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('心電図の所見'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: ecgController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('尿所見の内容'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: urineNoteController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Text('その他'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: TextField(
+                    controller: othersController,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 10),
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    Navigator.pop(context);
+                    currentMemo == null ? await save() : await upData();
+                  },
+                  child: Text(currentMemo == null ? '保存' : '編集終了')),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/memo_detail_screen.dart
+++ lib/screens/memo_detail_screen.dart
@@ -4,21 +4,226 @@ import 'package:memo/models/memo.dart';
 class MemoDetailPage extends StatelessWidget {
   static String id = 'memo_detail_screen';
   final Memos _memo;
-  const MemoDetailPage(this._memo, {Key? key}) : super(key: key);
+  MemoDetailPage(this._memo, {Key? key}) : super(key: key);
+  TextEditingController heightController = TextEditingController();
+  TextEditingController weightController = TextEditingController();
+  TextEditingController stateOfNutritionController = TextEditingController();
+  TextEditingController spinalColumnNoteController = TextEditingController();
+  TextEditingController rightEyeController = TextEditingController();
+  TextEditingController leftEyeController = TextEditingController();
+  TextEditingController rightCorrectedEyeController = TextEditingController();
+  TextEditingController leftCorrectedEyeController = TextEditingController();
+  TextEditingController eyeDiseaseController = TextEditingController();
+  TextEditingController earDiseaseController = TextEditingController();
+  TextEditingController tuberculosisController = TextEditingController();
+  TextEditingController tuberculosisDayController = TextEditingController();
+  TextEditingController ecgController = TextEditingController();
+  TextEditingController urineNoteController = TextEditingController();
+  TextEditingController othersController = TextEditingController();
+  TextEditingController other2Controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    heightController.text = _memo.height.toString();
+    weightController.text = _memo.weight.toString();
+    stateOfNutritionController.text = _memo.stateOfNutrition.toString();
+    spinalColumnNoteController.text = _memo.spinalColumnNote.toString();
+    rightEyeController.text = _memo.rightEye.toString();
+    leftEyeController.text = _memo.leftEye.toString();
+    rightCorrectedEyeController.text = _memo.rightCorrectedEye.toString();
+    leftCorrectedEyeController.text = _memo.leftCorrectedEye.toString();
+    eyeDiseaseController.text = _memo.eyeDisease.toString();
+    earDiseaseController.text = _memo.earDisease.toString();
+    tuberculosisController.text = _memo.tuberculosis.toString();
+    tuberculosisDayController.text = _memo.tuberculosisDay.toString();
+    ecgController.text = _memo.ecg.toString();
+    urineNoteController.text = _memo.urineNote.toString();
+    othersController.text = _memo.others.toString();
+    other2Controller.text = _memo.other2.toString();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(_memo.title),
       ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 15.0, left: 15.0, right: 8.0),
+        child: ListView(
           children: [
-            const Text('詳細'),
-            Text(_memo.title),
+            /*-身長体重----------------*/
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding : const EdgeInsets.all(10.0),
+                    child: TextField(
+                      controller: heightController,
+                      textAlign: TextAlign.right,
+                      decoration: const InputDecoration(
+                        labelText: '身長',
+                        suffix: Text('cm'),
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 5),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextField(
+                      controller: weightController,
+                      textAlign: TextAlign.right,
+                      decoration: const InputDecoration(
+                        labelText: '体重',
+                        suffix: Text('kg'),
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            ListTile(
+              title: TextField(
+                controller: stateOfNutritionController,
+                textAlign: TextAlign.right,
+                decoration: const InputDecoration(
+                  labelText: '栄養状態',
+                  filled: true,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            ListTile(
+              title: TextField(
+                controller: stateOfNutritionController,
+                textAlign: TextAlign.right,
+                decoration: const InputDecoration(
+                  labelText: '脊柱の病気',
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+
+            /*-視力関連----------------*/
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextField(
+                      controller: rightEyeController,
+                      textAlign: TextAlign.right,
+                      decoration: const InputDecoration(
+                        labelText: '右目（裸眼）',
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 5),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextField(
+                      controller: leftEyeController,
+                      textAlign: TextAlign.right,
+                      decoration: const InputDecoration(
+                        labelText: '左（裸眼）',
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextField(
+                      controller: rightCorrectedEyeController,
+                      textAlign: TextAlign.right,
+                      decoration: const InputDecoration(
+                        labelText: '右目（矯正）',
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 5.0),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextField(
+                      controller: leftCorrectedEyeController,
+                      textAlign: TextAlign.right,
+                      decoration: const InputDecoration(
+                        labelText: '左目（矯正）',
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ListTile(
+              title: TextField(
+                controller: eyeDiseaseController,
+                textAlign: TextAlign.right,
+                decoration: const InputDecoration(
+                  labelText: '目の病気',
+                  filled: true,
+                ),
+              ),
+            ),
+
+            ListTile(
+              title: TextField(
+                controller: earDiseaseController,
+                textAlign: TextAlign.right,
+                decoration: const InputDecoration(
+                  labelText: '耳の病気',
+                  filled: true,
+                ),
+              ),
+            ),
+
+            ListTile(
+              title: TextField(
+                controller: ecgController,
+                textAlign: TextAlign.right,
+                decoration: const InputDecoration(
+                  labelText: '心臓の病気',
+                  filled: true,
+                ),
+              ),
+            ),
+            ListTile(
+              title: TextField(
+                controller: urineNoteController,
+                textAlign: TextAlign.right,
+                decoration: const InputDecoration(
+                  labelText: '尿検査所見',
+                  filled: true,
+                ),
+              ),
+            ),
+
+            ListTile(
+              title: TextField(
+                controller: othersController,
+                textAlign: TextAlign.right,
+                decoration: const InputDecoration(
+                  labelText: 'その他',
+                  filled: true,
+                ),
+              ),
+            ),
           ],
         ),
       ),
